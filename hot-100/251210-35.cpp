6c8988d5380dc561d91070295665b5fbb7d1908d
Frank-whw <2120320012@qq.com>
2025-12-10 09:01:11 +0800

    25 12 10 搜索插入位置 二分 模板题

---
+++ hot-100/251210-35.cpp
@@ -0,0 +1,31 @@
+// 35. 搜索插入位置
+
+// 简单
+
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+// 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+// 请必须使用时间复杂度为 O(log n) 的算法。
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l < r)
+        {
+            int mid = l + (r - l) / 2;
+            if(nums[mid] < target){
+                l = mid + 1;
+            }else if(nums[mid] > target){
+                r = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+        return target > nums[l] ? l + 1 : l;
+        
+    }
+};
