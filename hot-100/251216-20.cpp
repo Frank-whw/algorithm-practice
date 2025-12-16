254adc368cccd81b9a2164a477da407bb18c84d7
Frank-whw <2120320012@qq.com>
2025-12-16 09:59:09 +0800

    25 12 16 有效的括号 栈

---
+++ hot-100/251216-20.cpp
@@ -0,0 +1,43 @@
+// 20. 有效的括号
+
+// 简单
+
+// 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+
+// 有效字符串需满足：
+
+// 左括号必须用相同类型的右括号闭合。
+// 左括号必须以正确的顺序闭合。
+// 每个右括号都有一个对应的相同类型的左括号。
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> st;
+        for (char c : s)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                st.push(c);
+            }
+            else
+            {
+                if (st.empty()) return false;
+                char t = st.top();
+                st.pop();
+                if (t == '(' && c == ')' || t == '{' && c == '}' || t == '[' && c == ']')
+                {
+                    continue;
+                }
+                else
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
