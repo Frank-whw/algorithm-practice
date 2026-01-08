e9e6ea08bca30fdbee565afe6f547546be48b9e0
Frank-whw <2120320012@qq.com>
2026-01-08 09:35:21 +0800

    26 01 08 爬楼梯 dp 简单

---
+++ hot-100/260108-70.cpp
@@ -0,0 +1,30 @@
+// 70. 爬楼梯
+
+// 简单
+
+// 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        vector<int> dp(n + 1, 0);
+        if (n <= 2)
+        {
+            return n;
+        }
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
