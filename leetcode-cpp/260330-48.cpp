63811a1465b4c17aa180d69d0541e8e80ca51d70
Frank-whw <2120320012@qq.com>
2026-03-30 20:17:46 +0800

    26 03 30 旋转图像

---
+++ leetcode-cpp/260330-48.cpp
@@ -0,0 +1,30 @@
+// 先转置 再镜像
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        // for (int i = 0; i < n; i++)
+        // {
+        //     for (int j = 0; j < n; j++)
+        //     {
+        //         cout<<matrix[i][j]<< " ";
+        //     }
+        //     cout << endl;
+            
+        // }
+        
+        for (int  i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n / 2; j++){
+                // cout << j << " " << n - 1 - j << endl;
+                swap(matrix[i][j], matrix[i][n - 1 - j]);
+            }
+        }
+        
+    }
+};
