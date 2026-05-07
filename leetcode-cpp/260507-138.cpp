4fbffc9c1fcb0cf174720d9c787a85f068b6e4c7
Frank-whw <2120320012@qq.com>
2026-05-07 19:44:50 +0800

    26 05 07 随机链表的复制

---
+++ leetcode-cpp/260507-138.cpp
@@ -0,0 +1,31 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*, Node*> mp;
+        for(auto p = head;p;p = p->next){
+            mp[p] = new Node(p->val);
+        }
+        for (auto p = head; p; p = p->next)
+        {
+            mp[p]->next = mp[p->next];
+            mp[p]->random = mp[p->random];
+        }
+        return mp[head];
+    }
+};
