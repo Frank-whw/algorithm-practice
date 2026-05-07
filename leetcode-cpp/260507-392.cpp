72afc1e0df6ec3f49205d0fe30f71e49c02a864a
Frank-whw <2120320012@qq.com>
2026-05-07 19:45:08 +0800

    26 05 07 判断子序列

---
+++ leetcode-cpp/260507-392.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int si = 0, ti = 0;
+        while(ti < t.size()){
+            if(s[si] == t[ti]){
+                si++;
+                ti++;
+            }else{
+                ti++;
+            }
+        }
+        return si == s.size();
+    }
+};
