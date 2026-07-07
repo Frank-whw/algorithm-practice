ae916827aafa52ca1ee788654637a5061d928bcb
Frank-whw <2120320012@qq.com>
2026-07-08 01:20:27 +0800

    26 07 07 岛屿数量

---
+++ hot-100/2607007-200.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    public:
+        void dfs(vector<vector<char>>& grid, int r, int c){
+            int nr = grid.size();
+            int nc = grid[0].size();
+    
+            grid[r][c] = '0';
+            if(r - 1 >= 0 && grid[r - 1][c] == '1') dfs(grid, r - 1, c);
+            if(r + 1 < nr && grid[r + 1][c] == '1') dfs(grid, r + 1, c);
+            if(c - 1 >= 0 && grid[r][c - 1] == '1') dfs(grid, r, c - 1);
+            if(c + 1 < nc && grid[r][c + 1] == '1') dfs(grid, r, c + 1);
+        }
+        int numIslands(vector<vector<char>>& grid) {
+            int nr = grid.size();
+            int nc = grid[0].size();
+    
+            int res = 0;
+            for(int i = 0; i < nr; i++) {
+                for(int j = 0; j < nc; j++){
+                    if(grid[i][j] == '1'){
+                        res ++;
+                        dfs(grid, i, j);
+                    }
+                }
+            }
+            return res;
+        }
+    };
